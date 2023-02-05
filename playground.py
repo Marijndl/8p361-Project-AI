bd0bd146e29285d3673178129a67a6d40a215413
Marijn <51094560+Marijndl@users.noreply.github.com>
2023-02-05 20:39:20 +0100

    Open test image

---
+++ playground.py
@@ -0,0 +1,8 @@
+# Imports PIL module
+from PIL import Image
+
+# open method used to open different extension image file
+im = Image.open(r"C:\Users\20203226\Documents\GitHub\8p361_images\test\0000ec92553fda4ce39889f9226ace43cae3364e.tif")
+
+# This method will show image in any image viewer
+im.show()
